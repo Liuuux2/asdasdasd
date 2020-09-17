7055587579f073976e7cba1b6727ec798c8807e8
Liuuux2 <71445580+Liuuux2@users.noreply.github.com>
2020-09-17 12:38:43 -0600

    Create starvation.py

---
+++ starvation.py
@@ -0,0 +1,8 @@
+import socket
+header = ('Tumama es tanga')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+s.settimeout(0)
+s.connect(('209.222.97.89', 25565))
+s.send(header)
+s.close() 
