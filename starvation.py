a850a69fcd620aced1ce578583ccd886f11ef128
Liuuux2 <71445580+Liuuux2@users.noreply.github.com>
2020-09-17 12:50:01 -0600

    Update starvation.py

--- starvation.py
+++ starvation.py
@@ -3,7 +3,7 @@ import socket
 header = ('Tumama es tanga')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-s.settimeout(1)
+s.settimeout(5)
 s.connect(('209.222.97.89', 25565))
 s.send(header)
 s.close() 
